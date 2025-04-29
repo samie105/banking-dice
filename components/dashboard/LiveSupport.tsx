aa6e4d2e56a688ea712d705df5545f5bbed12e17
samie105 <samsonrichfield@gmail.com>
2025-04-29 16:31:08 +0100

    jhghj

--- components/dashboard/LiveSupport.tsx
+++ components/dashboard/LiveSupport.tsx
@@ -4,8 +4,7 @@ import Script from "next/script";
 export default function LiveSupport() {
 
 
-  return <>
-  <Script src="//code.jivosite.com/widget/1U5DmgXFzL" async/>
+  return  <Script src="//code.jivosite.com/widget/1U5DmgXFzL" async/>
 
-  </>;
+
 }
